942129119a2fd0f0fb99b72e8e7271f01dd8a5b7
Daniela90-prog <116611137+Daniela90-prog@users.noreply.github.com>
2025-01-06 23:40:08 -0300

    Update Daniela90-prog.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Daniela90-prog.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Daniela90-prog.py
@@ -1,1 +1,28 @@
+# esto es un comentario
+
+"""
+esto es mi comentario 
+en varias lineas 
+"""
+
+'''
+esto es mi comentario
+en varias lineas 
+'''
+
+mi_primera_variable = "Mi variable"
+mi_primera_variable = "Nuevo valor de mi variable"
+
+MI_CONSTANTE = "Mi constante"
+
+# Datos primitivos
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("¡Hola, Python!")
 
